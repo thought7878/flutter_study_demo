623d5d5c21f46a05ab3cadf2397399a5ecbf65b3
Calm Andy <thought7878@qq.com>
2019-06-28 09:34:35 +0800

    列表项目

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,35 @@ class App extends StatelessWidget {
 
 class Home extends StatelessWidget {
   Widget _listItemBuilder(BuildContext context, int index) {
-    return Text(
-      posts[index].title,
+    return Container(
+      margin: EdgeInsets.all(8),
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(
+            posts[index].author,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(
+            height: 16,
+          )
+        ],
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
         title: Text('hello flutter'),
         elevation: 0,
