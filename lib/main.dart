9272b61f490a769cf019485d2e345c03659e0528
Calm Andy <thought7878@qq.com>
2019-06-28 14:43:19 +0800

    Drawer：在抽屉里使用 ListView， DrawerHeader，ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -25,68 +25,103 @@ class Home extends StatelessWidget {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'navigation',
-            onPressed: () => debugPrint('navigation button pressed'),
-          ),
-          title: Text('hello flutter'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'search',
-              onPressed: () => debugPrint('search button is pressed'),
-            )
-          ],
-          elevation: 0,
-          bottom: TabBar(
-            unselectedLabelColor: Colors.black38,
-            indicatorColor: Colors.black54,
-            indicatorSize: TabBarIndicatorSize.label,
-            indicatorWeight: 1,
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.security),
-              ),
-              Tab(
-                icon: Icon(Icons.send),
-              ),
-              Tab(
-                icon: Icon(Icons.settings),
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'navigation',
+              onPressed: () => debugPrint('navigation button pressed'),
+            ),
+            title: Text('hello flutter'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'search',
+                onPressed: () => debugPrint('search button is pressed'),
               )
             ],
+            elevation: 0,
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38,
+              indicatorColor: Colors.black54,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorWeight: 1,
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.security),
+                ),
+                Tab(
+                  icon: Icon(Icons.send),
+                ),
+                Tab(
+                  icon: Icon(Icons.settings),
+                )
+              ],
+            ),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            Icon(
-              Icons.security,
-              size: 128,
-              color: Colors.black12,
-            ),
-            Icon(
-              Icons.send,
-              size: 128,
-              color: Colors.black12,
-            ),
-            Icon(
-              Icons.settings,
-              size: 128,
-              color: Colors.black12,
-            ),
-          ],
-        ),
-        drawer: Container(
-          color: Colors.white,
-          padding: EdgeInsets.all(8),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Text('this is a drawer')],
+          body: TabBarView(
+            children: <Widget>[
+              Icon(
+                Icons.security,
+                size: 128,
+                color: Colors.black12,
+              ),
+              Icon(
+                Icons.send,
+                size: 128,
+                color: Colors.black12,
+              ),
+              Icon(
+                Icons.settings,
+                size: 128,
+                color: Colors.black12,
+              ),
+            ],
           ),
-        ),
-      ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Text('drawer header'.toUpperCase()),
+                  decoration: BoxDecoration(color: Colors.grey[100]),
+                ),
+                ListTile(
+                  title: Text(
+                    'Message',
+                    textAlign: TextAlign.right,
+                  ),
+                  trailing: Icon(
+                    Icons.message,
+                    color: Colors.black12,
+                    size: 22,
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    'Favorite',
+                    textAlign: TextAlign.right,
+                  ),
+                  trailing: Icon(
+                    Icons.favorite,
+                    color: Colors.black12,
+                    size: 22,
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    'Settings',
+                    textAlign: TextAlign.right,
+                  ),
+                  trailing: Icon(
+                    Icons.settings,
+                    color: Colors.black12,
+                    size: 22,
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
