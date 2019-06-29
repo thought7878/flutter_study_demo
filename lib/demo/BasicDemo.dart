f53d510ff713a0dc57c74a39c99144279c4e7336
Calm Andy <thought7878@qq.com>
2019-06-29 10:24:40 +0800

    RichText：行内多样式的文字

--- lib/demo/BasicDemo.dart
+++ lib/demo/BasicDemo.dart
@@ -8,6 +8,38 @@ class BasicDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        text: 'github',
+        style: TextStyle(
+          color: Colors.green,
+          fontSize: 40,
+          fontWeight: FontWeight.w900,
+        ),
+        children: [
+          TextSpan(
+            text: '.com',
+            style: TextStyle(
+              color: Colors.orange,
+              fontSize: 20,
+              fontWeight: FontWeight.w100,
+              fontStyle: FontStyle.italic,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TextDemo extends StatelessWidget {
+  final TextStyle _textStyle = TextStyle(fontSize: 16, color: Colors.green);
+
+  final String title = '将进酒';
+  final String author = '李白';
+
+  @override
+  Widget build(BuildContext context) {
     return Text(
       '《$title》--- $author。 君不见黄河之水天上来，奔流到海不复回。君不见高堂明镜悲白发，朝如青丝暮成雪。人生得意须尽欢，莫使金樽空对月。天生我材必有用，千金散尽还复来。烹羊宰牛且为乐，会须一饮三百杯。',
       textAlign: TextAlign.left,
