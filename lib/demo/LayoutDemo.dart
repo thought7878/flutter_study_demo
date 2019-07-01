0b7885efacf6e9d196997e55c25839bceac14bc9
Calm Andy <thought7878@qq.com>
2019-07-01 17:33:56 +0800

    Stack：一摞小部件

--- lib/demo/LayoutDemo.dart
+++ lib/demo/LayoutDemo.dart
@@ -7,40 +7,72 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          SizedBox(
-            width: 200.0,
-            height: 300.0,
-            child: Container(
-              // alignment: Alignment(0.0, 0.0),
-              alignment: Alignment.topLeft,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+          Stack(
+            alignment: Alignment.topRight,
+            children: <Widget>[
+              SizedBox(
+                width: 200.0,
+                height: 300.0,
+                child: Container(
+                  // alignment: Alignment(0.0, 0.0),
+                  alignment: Alignment.topCenter,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(3, 54, 255, 1.0),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Icon(
+                    Icons.ac_unit,
+                    color: Colors.white,
+                    size: 32.0,
+                  ),
+                ),
               ),
-              child: Icon(
-                Icons.ac_unit,
-                color: Colors.white,
-                size: 32.0,
+              SizedBox(
+                height: 32.0,
               ),
-            ),
-          ),
-          SizedBox(
-            height: 50.0,
-          ),
-          SizedBox(
-            width: 100.0,
-            height: 100.0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+              SizedBox(
+                width: 100.0,
+                height: 100.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Icon(
+                    Icons.access_alarm,
+                    color: Colors.white,
+                    size: 32.0,
+                  ),
+                ),
               ),
-              child: Icon(
-                Icons.access_alarm,
-                color: Colors.white,
-                size: 32.0,
+              Positioned(
+                left: 10.0,
+                top: 10.0,
+                child: Icon(
+                  Icons.ac_unit,
+                  color: Colors.white,
+                  size: 16.0,
+                ),
               ),
-            ),
+              Positioned(
+                left: 10.0,
+                top: 30.0,
+                child: Icon(
+                  Icons.ac_unit,
+                  color: Colors.white,
+                  size: 16.0,
+                ),
+              ),
+              Positioned(
+                left: 10.0,
+                top: 50.0,
+                child: Icon(
+                  Icons.ac_unit,
+                  color: Colors.white,
+                  size: 16.0,
+                ),
+              ),
+            ],
           )
         ],
       ),
