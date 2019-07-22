aeb6bb4204b6d11ae87587379416780ea25d8b77
Calm Andy <thought7878@qq.com>
2019-07-22 14:12:01 +0800

    StatefulWidget：带变化状态的小部件

--- lib/demo/state/StateManagementDemo.dart
+++ lib/demo/state/StateManagementDemo.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class StateManagementDemo extends StatelessWidget {
-  StateManagementDemo({Key key}) : super(key: key);
+class StateManagementDemo extends StatefulWidget {
+  @override
+  _StateManagementDemoState createState() {
+    return _StateManagementDemoState();
+  }
+}
 
+class _StateManagementDemoState extends State<StateManagementDemo> {
   int count = 0;
 
   @override
@@ -10,6 +15,33 @@ class StateManagementDemo extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('StateManagementDemo'),
+      ),
+      body: Center(
+        child: Chip(
+          label: Text('$count'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            count += 1;
+          });
+          print(count);
+        },
+      ),
+    );
+  }
+}
+
+class StatelessDemo extends StatelessWidget {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('StatelessDemo'),
       ),
       body: Center(
         child: Chip(
